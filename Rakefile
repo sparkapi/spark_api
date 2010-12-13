200736c9e0bec8ec54fac082261b0bce2676cc47
Wade McEwen <wade@fbsdata.com>
2010-12-13 11:22:28 -0600

    CUR-10533 fixing rake tasks

--- Rakefile
+++ Rakefile
@@ -35,3 +35,16 @@ end
 desc "Run all the tests"
 task :default => :spec
 
+Rake::TaskManager.class_eval do
+  def remove_task(task_name)
+    @tasks.delete(task_name.to_s)
+  end
+end
+
+def remove_task(task_name)
+  Rake.application.remove_task(task_name)
+end 
+
+remove_task 'release'
+remove_task 'rubygems:release'
+
--- spec/unit/flexmls_api_spec.rb
+++ spec/unit/flexmls_api_spec.rb
@@ -9,3 +9,16 @@ describe FlexmlsApi do
   end
 
 end
+
+describe FlexmlsApi::Client do
+  describe "connect" do
+
+    it "should authenticate" do
+      client = FlexmlsApi::Client.new("TopSecret", "key_of_wade", "http://api.wade.dev.fbsdata.com")
+      client.authenticate.should match(/\d+\.\d+\.\d+/)
+    end
+
+  end
+
+end
+
