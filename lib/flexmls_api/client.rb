74375c28c324f037a57e54f22403b892561f596f
Brandon HOrnseth <bhornseth@fbsdata.com>
2010-12-14 10:55:17 -0600

    Removed Debugging output for the time being

--- lib/flexmls_api/client.rb
+++ lib/flexmls_api/client.rb
@@ -1,15 +1,6 @@
 module FlexmlsApi
   class Client < Base
     include Authentication
-    #def initialize(*args)
-    #  super(args)
-    #  puts "Settings: "
-    #  puts "  api_key = #{self.api_key}"
-
-    #  #@secret = secret
-    #  #@key = key
-    #  #@url = url
-    #end
 
   end
 end
--- lib/flexmls_api/configuration.rb
+++ lib/flexmls_api/configuration.rb
@@ -12,23 +12,12 @@ module FlexmlsApi
     attr_accessor *VALID_OPTION_KEYS
     def configure
       yield self
-      puts "Inspect: #{self.inspect}"
     end
 
     def self.extended(base)
       base.reset
     end
 
-    def show_settings
-      puts "Settings:"
-      puts "========================"
-      puts "ApiKey:      #{self.api_key}"
-      puts "ApiSecret:   #{self.api_secret}"
-      puts "Endpoint:    #{self.endpoint}"
-      puts "Version:     #{self.version}"
-      puts "UA:          #{self.user_agent}"
-
-    end
 
     def options
       VALID_OPTION_KEYS.inject({}) do |opt,key|
@@ -38,13 +27,11 @@ module FlexmlsApi
 
 
     def reset
-      puts "============Called Reset============"
       self.api_key     = DEFAULT_API_KEY
       self.api_secret  = DEFAULT_API_SECRET
       self.endpoint    = DEFAULT_ENDPOINT
       self.version     = DEFAULT_VERSION
       self.user_agent  = DEFAULT_USER_AGENT
-      puts show_settings
       self
     end
   end
