fcacd1d020000a2d4bd38053d3e15f89b579aeaf
Wade McEwen <wade@fbsdata.com>
2010-12-23 14:39:07 -0600

    Contact model

--- lib/flexmls_api/models.rb
+++ lib/flexmls_api/models.rb
@@ -5,6 +5,7 @@ require File.expand_path('../models/system_info', __FILE__)
 require File.expand_path('../models/standard_fields', __FILE__)
 require File.expand_path('../models/property_types', __FILE__)
 require File.expand_path('../models/connect_prefs', __FILE__)
+require File.expand_path('../models/contact', __FILE__)
 
 module FlexmlsApi
   module Models
