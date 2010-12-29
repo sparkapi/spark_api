6df50c8fc3faf63120ff2ed22c49a663fe5a11e4
Brandon Hornseth <bhornseth@fbsdata.com>
2010-12-29 11:30:53 -0600

    added Photo.find_by_listing_key

--- lib/flexmls_api/models/photo.rb
+++ lib/flexmls_api/models/photo.rb
@@ -1,10 +1,16 @@
 module FlexmlsApi
   module Models
     class Photo < Model
-      
 
       def primary? 
         @attributes["Primary"] == true 
+      end
+
+      def self.find_by_listing_key(key)
+        photos = []
+        resp = FlexmlsApi.client.get("/listings/#{key}/photos")
+        resp.collect { |photo| photos.push(new(photo)) }
+        photos
       end
 
     end
