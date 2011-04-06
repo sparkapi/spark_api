70e39a93c6fb6dc0602ff052d24961e5742b38f7
Brandon Medenwald <brandon@fbsdata.com>
2011-04-06 10:05:31 -0500

    Change owner param to arguments hash

--- lib/flexmls_api/models/standard_fields.rb
+++ lib/flexmls_api/models/standard_fields.rb
@@ -5,13 +5,13 @@ module FlexmlsApi
       self.element_name="standardfields"
       
       # expand all fields passed in
-      def self.find_and_expand_all(fields, owner)
+      def self.find_and_expand_all(fields, arguments={})
         returns = {}
         
         # find all standard fields, but expand only the location fields
         # TODO: when _expand support is added to StandardFields API, use the following
         # standard_fields = find(:all, :ApiUser => owner, :_expand => fields.join(",")) 
-        standard_fields = find(:all, :ApiUser => owner) 
+        standard_fields = find(:all, arguments) 
       
         # filter through the list and return only the location fields found
         fields.each { |field|
@@ -21,7 +21,7 @@ module FlexmlsApi
               
             # lookup fully _expand fileld, if the field has a list
             if returns[field]['HasList']
-              returns[field] = connection.get("/standardfields/#{field}", :ApiUser => owner).first[field]
+              returns[field] = connection.get("/standardfields/#{field}", arguments).first[field]
             end
               
           end
