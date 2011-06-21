d922a182dc5f4419e4e549ec832a6127c5936b2d
Wade McEwen <wade@fbsdata.com>
2011-06-21 14:06:03 -0500

    API-142  Added put / delete functionality.

--- spec/unit/flexmls_api/models/listing_cart_spec.rb
+++ spec/unit/flexmls_api/models/listing_cart_spec.rb
@@ -27,7 +27,7 @@ describe ListingCart do
     resources.first.Id.should eq("20100912153422758914000000")
   end
   
-  it "should save a listing cart" do
+  it "should save a new listing cart" do
     stub_api_post("/#{subject.class.element_name}", 'listing_cart_new.json', 'listing_cart_post.json')
     subject.ListingIds = [
       '20110112234857732941000000',
@@ -38,6 +38,20 @@ describe ListingCart do
     subject.ResourceUri.should eq("/v1/listingcarts/20100912153422758914000000")
   end
 
+  it "should save a listing cart" do
+    stub_api_get("/#{subject.class.element_name}", 'listing_cart.json')
+    resource = subject.class.get.first
+    stub_api_put("/#{subject.class.element_name}/#{resource.Id}", 'listing_cart_new.json', 'success.json')
+    resource.ListingIds = [
+      '20110112234857732941000000',
+      '20110302120238448431000000',
+      '20110510011212354751000000']
+    
+    resource.Name = "My Cart's Name"
+    resource.save.should be(true)
+    resource.ResourceUri.should eq("/v1/listingcarts/20100912153422758914000000")
+  end
+  
   it "should fail saving" do
     stub_api_post("/#{subject.class.element_name}",'listing_cart_empty.json') do |request|
       request.to_return(:status => 400, :body => fixture('errors/failure.json'))
@@ -45,6 +59,16 @@ describe ListingCart do
     subject
     subject.save.should be(false)
     expect{ subject.save! }.to raise_error(FlexmlsApi::ClientError){ |e| e.status.should == 400 }
+  end
+
+  it "should delete a listing cart" do
+    stub_api_get("/#{subject.class.element_name}", 'listing_cart.json')
+    resource = subject.class.get.first
+    resource.Id.should eq("20100912153422758914000000")
+    resource.Name.should eq("My Listing Cart")
+    resource.ListingCount.should eq(10)
+    stub_api_delete("/#{subject.class.element_name}/#{resource.Id}", 'success.json')
+    resource.delete.should be(nil)
   end
 
 end
