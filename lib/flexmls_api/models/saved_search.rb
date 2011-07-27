57ddd65d44cd312b45e7fb17380725a7819e7575
Nick Larson <nick@fbsdata.com>
2011-07-27 13:54:09 -0500

    provided saved searches

--- lib/flexmls_api/models/saved_search.rb
+++ lib/flexmls_api/models/saved_search.rb
@@ -3,6 +3,14 @@ module FlexmlsApi
     class SavedSearch < Base 
       extend Finders
       self.element_name="savedsearches"
+
+      def self.provided()
+        Class.new(self).tap do |provided|
+          provided.element_name = '/savedsearches'
+          provided.prefix = '/provided'
+          FlexmlsApi.logger.info("#{self.name}.path: #{provided.path}")
+        end
+      end
     end
   end
 end
--- spec/unit/flexmls_api/models/saved_search_spec.rb
+++ spec/unit/flexmls_api/models/saved_search_spec.rb
@@ -8,7 +8,7 @@ describe SavedSearch do
 
   let(:id){ "20100815220615294367000000" }
     
-  it "should get all SavedSearchs" do
+  it "should get all SavedSearches" do
     stub_api_get("/#{subject.class.element_name}", 'saved_search.json')
     resources = subject.class.get
     resources.should be_an(Array)
@@ -21,6 +21,14 @@ describe SavedSearch do
     resource = subject.class.find(id)
     resource.Id.should eq(id)
     resource.Name.should eq("Search name here")
+  end
+
+  it "should get provided SavedSearches" do
+    stub_api_get("/provided/#{subject.class.element_name}", 'saved_search.json')
+    resources = subject.class.provided.get
+    resources.should be_an(Array)
+    resources.length.should eq(2)
+    resources.first.Id.should eq(id)
   end
 
 end
