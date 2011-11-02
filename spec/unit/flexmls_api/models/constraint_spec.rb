5b6b739a28ecd1df7e9a2889198c61c73825adb7
Brandon Hornseth <bhornseth@fbsdata.com>
2011-11-02 13:08:25 -0500

    Revert "modify the to_s method for Constraints"
    This reverts commit 2c83c40465dcd85f59065b197d63f585ab8269a0.

--- spec/unit/flexmls_api/models/constraint_spec.rb
+++ spec/unit/flexmls_api/models/constraint_spec.rb
@@ -5,15 +5,15 @@ describe Constraint do
   
   subject do
     Constraint.new(
-      "RuleValue" => 1,
-      "Value" => 0,
-      "RuleFieldValue" => nil,
-      "RuleField" => nil,
-      "RuleName" => "MinValue")
+      "RuleValue" => 1000000.0,
+      "Value" => 1000001.0,
+      "RuleFieldValue" => 1.0,
+      "RuleField" => "ListPrice",
+      "RuleName" => "MaxValue")
   end
   
   it "should print to string" do
-    subject.to_s.should eq("The minimum value for this field is 1")
+    subject.to_s.should eq("MaxValue: Field(ListPrice,1.0) Value(1000000.0,1000001.0)")
   end
 
 end
