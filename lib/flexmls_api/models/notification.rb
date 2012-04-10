685e984c405328ea214beac4b24d0a00ccb604c4
Matthew O'Brien <mobrien@fbsdata.com>
2012-04-10 14:27:18 -0500

    RUBYAPI-15: Notifications support

---
+++ lib/flexmls_api/models/notification.rb
@@ -0,0 +1,42 @@
+module FlexmlsApi
+  module Models
+    class Notification < Base
+
+      self.element_name = 'notifications'
+
+      def save(arguments={})
+        self.errors = [] # clear the errors hash
+        begin
+          return save!(arguments)
+        rescue BadResourceRequest => e
+          self.errors << {:code => e.code, :message => e.message}
+          FlexmlsApi.logger.error("Failed to save resource #{self}: #{e.message}")
+        rescue NotFound => e
+          FlexmlsApi.logger.error("Failed to save resource #{self}: #{e.message}")
+        end
+        false
+      end
+
+      def save!(arguments={})
+        results = connection.post self.class.path, attributes, arguments
+        result = results.first
+        attributes['ResourceUri'] = result['ResourceUri']
+        true
+      end
+
+      def self.unread()
+        # force pagination so response knows to deal with returned pagination info
+        result = connection.get "#{self.path}/unread", {:_pagination => 'count'}
+        result
+      end
+
+      def self.mark_read(notifications, arguments={})
+        notifications = Array(notifications)
+
+        ids = notifications.map { |n| n.respond_to?('Id') ? n.Id : n }
+        result = connection.put "#{self.path}/#{ids.join(',')}", {'Read' => true}, arguments
+      end
+
+    end
+  end
+end
