fee4dca8e1bfc7558c4fe38027041877d4d5674a
Nick Larson <nick.larson@gmail.com>
2012-10-19 11:10:48 -0500

    FLEX-31: add tests

--- spec/unit/spark_api/models/subscriptions.rb
+++ spec/unit/spark_api/models/subscriptions.rb
@@ -12,26 +12,40 @@ describe Subscription do
     stub_auth_request
   end
 
-  subject do
-    Subscription.new({
-      :SearchId => 1,
-      :RecipientIds => ["20000426173054342350000000"],
-      :Subject => "Test subject",
-      :Message => "This could be a message."
-    })
-  end
+  let(:id){ "20101230223226074204000000" }
 
   context "/subscriptions", :support do
+
     on_get_it "should get sum subscriptions"
 
     on_post_it "should create a new subscription" do
       stub_api_post("/subscriptions", @subscription.attributes.as_json, 'subscriptions/post.json')
       subject.save.should be(true)
+      subject.persisted?.should eq(true)
+    end
+
+  end
+
+  context "/subscriptions/:id", :support do
+
+    on_get_it "should get a subscription" do
+      stub_api_get("/subscriptions/#{id}", "subscriptions/get.json")
+      subject.class.find(id)
     end
 
     on_put_it "should update a subscription" do
-      stub_api_put("/subscriptions", @subscription.attributes.as_json, 'subscriptions/post.json')
+      stub_api_get("/subscriptions/#{id}", 'subscriptions/get.json')
+      stub_api_put("/subscriptions/#{id}", 'subscriptions/update.json', 'subscriptions/post.json')
+      resource = subject.class.get
+      resource.Name = "A new subscription name"
       subject.save.should be(true)
+    end
+
+    on_delete_it "should delete a subscription" do
+      stub_api_get("/subscriptions/#{id}", 'subscriptions/get.json')
+      stub_api_delete("/subscriptions/#{id}", 'generic_delete.json')
+      resource = subject.class.get
+      resource.delete
     end
 
   end
