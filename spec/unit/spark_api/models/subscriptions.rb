127ef1946b8ba63ca80c33e9faea394f8ef17fb2
Nick Larson <nick.larson@gmail.com>
2012-10-19 17:27:18 -0500

    FLEX-31/#16: add more tests for subscribing/unsubscribing contacts

--- spec/unit/spark_api/models/subscriptions.rb
+++ spec/unit/spark_api/models/subscriptions.rb
@@ -16,12 +16,49 @@ describe Subscription do
 
   context "/subscriptions", :support do
 
-    on_get_it "should get sum subscriptions"
+    on_get_it "should get sum subscriptions" do
+      stub_api_get("/subscriptions", "subscriptions/get.json")
+      subject.get
+    end
 
     on_post_it "should create a new subscription" do
       stub_api_post("/subscriptions", @subscription.attributes.as_json, 'subscriptions/post.json')
       subject.save.should be(true)
       subject.persisted?.should eq(true)
+    end
+
+    it "should subscribe a contact by id" do
+      stub_api_get("/subscriptions", "subscriptions/get.json")
+      stub_api_put("/subscriptions/#{id}/subscribers/20101230223226074306000000", {}, 'subscriptions/subscribe_unsubscribe.json')
+      resource = subject.find(id)
+      resource.subscribe("20101230223226074306000000")
+      resource.RecipientIds.size.should eq(1)
+      resource.RecipientIds.find { |c| c == "20101230223226074306000000" }.should eq(true)
+    end
+
+    it "should unsubscribe a contact by id" do
+      stub_api_get("/subscriptions", "subscriptions/get.json")
+      stub_api_post("/subscriptions/#{id}/subscribers/20101230223226074307000000", {}, 'subscriptions/subscribe_unsubscribe.json')
+      resource = subject.find(id)
+      resource.unsubscribe("20101230223226074307000000")
+      resource.RecipientIds.size.should eq(0)
+    end
+
+    it "should subscribe a contact by Contact object" do
+      stub_api_get("/subscriptions", "subscriptions/get.json")
+      stub_api_put("/subscriptions/#{id}/subscribers/20101230223226074306000000", {}, 'subscriptions/subscribe_unsubscribe.json')
+      resource = subject.find(id)
+      resource.subscribe(Contact.new({ :Id => "20101230223226074306000000" }))
+      resource.RecipientIds.size.should eq(1)
+      resource.RecipientIds.find { |c| c == "20101230223226074306000000" }.should eq(true)
+    end
+
+    it "should unsubscribe a contact by Contact object" do
+      stub_api_get("/subscriptions", "subscriptions/get.json")
+      stub_api_post("/subscriptions/#{id}/subscribers/20101230223226074307000000", {}, 'subscriptions/subscribe_unsubscribe.json')
+      resource = subject.find(id)
+      resource.unsubscribe(Contact.new({ :Id => "20101230223226074307000000" }))
+      resource.RecipientIds.size.should eq(0)
     end
 
   end
