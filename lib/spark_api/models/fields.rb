3c105131daf3118a5ee043112164a3aa7341a4be
Brandon Medenwald <brandon@fbsdata.com>
2013-01-02 10:31:18 -0600

    MOBILE-434: Change parameter name from card_fmt to property_type

--- lib/spark_api/models/fields.rb
+++ lib/spark_api/models/fields.rb
@@ -3,8 +3,8 @@ module SparkApi
     class Fields < Base
       self.element_name="fields"
 
-      def self.order(card_fmt=nil, arguments={})
-        connection.get("#{self.path}/order#{"/"+card_fmt unless card_fmt.nil?}", arguments)
+      def self.order(property_type=nil, arguments={})
+        connection.get("#{self.path}/order#{"/"+property_type unless property_type.nil?}", arguments)
       end
 
     end
