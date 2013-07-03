578257f2559d3b8b08d7e613bed9e7b871e5cda6
Nick Larson <nick.larson@gmail.com>
2013-07-03 09:45:03 -0500

    API-1544: return nil if no idx link

--- spec/unit/spark_api/models/idx_link_spec.rb
+++ spec/unit/spark_api/models/idx_link_spec.rb
@@ -6,11 +6,23 @@ describe IdxLink do
     stub_auth_request
   end
 
-  it "gets a user's default link" do
-    stub_api_get("/idxlinks/default", "idx_links/get.json")
-    @idx_link = subject.class.default
-    @idx_link.should be_an(IdxLink)
-    @idx_link.Uri.should eq("http://link.dev.fbsdata.com/zq1fkiw4d3f,1")
+  describe "/idxlinks/default" do
+
+    on_get_it "returns an agent's default idx link" do
+      s = stub_api_get("/idxlinks/default", "idx_links/get.json")
+      @idx_link = subject.class.default
+      @idx_link.should be_an(IdxLink)
+      @idx_link.Uri.should eq("http://link.dev.fbsdata.com/zq1fkiw4d3f,1")
+      s.should have_been_requested
+    end
+
+    on_get_it "returns nil if the user doesn't have one" do
+      s = stub_api_get("/idxlinks/default", "no_results.json")
+      @idx_link = subject.class.default
+      @idx_link.should be_nil
+      s.should have_been_requested
+    end
+
   end
 
 end
