96da22660dabba531a515617edb9e716a9a36fd8
Ryan Hertz <rhertz@fbsdata.com>
2016-05-18 11:00:34 -0400

    fixed bug in NewsFeedMeta core_fields and core_field_names Calling either of those methods modified the data array when it should have only been reading the array. Later calls to these methods were then incorrect.

--- spec/unit/spark_api/models/news_feed_meta_spec.rb
+++ spec/unit/spark_api/models/news_feed_meta_spec.rb
@@ -22,12 +22,26 @@ describe NewsFeedMeta do
         "Year Built", "Total SqFt.", "Sub Type", "Subdivision", "Map Area"]
       expect(news_feed_meta.core_field_names).to eq field_array    
     end
+
+    it "doesn't modify the original fields and returns the same results when run twice" do
+      field_array = ["Location", "Status", "Property Type", "Postal Code", "List Price", "Total Bedrooms", 
+        "Year Built", "Total SqFt.", "Sub Type", "Subdivision", "Map Area"]
+      expect(news_feed_meta.core_field_names).to eq field_array    
+      expect(news_feed_meta.core_field_names).to eq field_array    
+    end
   end
 
   describe 'core_fields' do
     it 'returns an array including both the CoreSearchFields and the CoreStandardFields' do
       field_array = ["Location", "MlsStatus", "PropertyType", "PostalCode", "ListPrice", "BedsTotal", 
         "YearBuilt", "BuildingAreaTotal", "PropertySubType", "SubdivisionName", "MLSAreaMinor"]
+      expect(news_feed_meta.core_fields).to eq field_array    
+    end
+
+    it "doesn't modify the original fields and returns the same results when run twice" do
+      field_array = ["Location", "MlsStatus", "PropertyType", "PostalCode", "ListPrice", "BedsTotal", 
+        "YearBuilt", "BuildingAreaTotal", "PropertySubType", "SubdivisionName", "MLSAreaMinor"]
+      expect(news_feed_meta.core_fields).to eq field_array    
       expect(news_feed_meta.core_fields).to eq field_array    
     end
   end
