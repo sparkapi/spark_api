9011888abf73822cd6cbbd5bfbf2c9fbc53eeb55
Brandon Hornseth <bhornseth@gmail.com>
2018-06-25 21:32:23 -0500

    Uncap RSpec, squash deprections for latest

--- spec/unit/spark_api/models/rental_calendar_spec.rb
+++ spec/unit/spark_api/models/rental_calendar_spec.rb
@@ -17,14 +17,16 @@ describe RentalCalendar do
   end
   
   describe "test include_date method" do
-    cal = RentalCalendar.new
-    cal.StartDate = Date.parse("2012-07-12")
-    cal.EndDate = Date.parse("2012-07-18")
-    cal.include_date?(Date.parse("2012-06-01")).should == false
-    cal.include_date?(Date.parse("2012-07-12")).should == true
-    cal.include_date?(Date.parse("2012-07-15")).should == true
-    cal.include_date?(Date.parse("2012-07-18")).should == true
-    cal.include_date?(Date.parse("2012-08-01")).should == false
+    it "knows about included dates" do
+      cal = RentalCalendar.new
+      cal.StartDate = Date.parse("2012-07-12")
+      cal.EndDate = Date.parse("2012-07-18")
+      expect(cal.include_date?(Date.parse("2012-06-01"))).to be false
+      expect(cal.include_date?(Date.parse("2012-07-12"))).to be true
+      expect(cal.include_date?(Date.parse("2012-07-15"))).to be true
+      expect(cal.include_date?(Date.parse("2012-07-18"))).to be true
+      expect(cal.include_date?(Date.parse("2012-08-01"))).to be false
+    end
   end
 
 end
