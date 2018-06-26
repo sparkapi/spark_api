83fd57af46194adb7c267490a3b057a5e2674e54
Brandon Hornseth <bhornseth@gmail.com>
2018-06-25 21:04:32 -0500

    spec_helper cleanup
    Take a chainsaw to some cruft that's accrued in here over the years
     - Coverage block can be simplified and shortened
     - We don't need to monkey patch a logger in anymore
     - don't re-require gems named in the gemspec
     - Drop the load path manipulation
     - rspec 3 uses `config.color`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,54 +1,25 @@
-if ENV['COVERAGE'] == "on"
-  require 'simplecov'
-  require 'simplecov-rcov'
-  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
-  SimpleCov.start do
-    add_filter '/vendor'
-    add_filter '/spec'
-    add_filter '/test'
-  end
-end
-
 require "rubygems"
 require "rspec"
 require 'rspec/autorun'
 require 'webmock/rspec'
-require "json"
-require 'multi_json'
-
-path = File.expand_path(File.dirname(__FILE__) + "/../lib/")
-$LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
-require path + '/spark_api'
-
 require 'spark_api'
+
+if ENV['COVERAGE'] == "on"
+  require 'simplecov-rcov'
+  SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+  SimpleCov.start { add_filter %w(/vendor /spec /test) }
+end
 
 FileUtils.mkdir 'log' unless File.exists? 'log'
 
-module SparkApi
-  def self.logger
-    if @logger.nil?
-      @logger = Logger.new('log/test.log')
-      @logger.level = Logger::DEBUG
-    end
-    @logger
-  end
-end
-
-SparkApi.logger.info("Setup gem for rspec testing")
-
 include SparkApi::Models
-
-def reset_config
-  SparkApi.reset
-  SparkApi.configure { |c| c.api_user = "foobar" }
-end
+SparkApi.logger = ::Logger.new('log/test.log')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # # in spec/support/ and its subdirectories.
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 RSpec.configure do |config|
-  
   config.include WebMock::API
   config.include StubApiRequests
 
@@ -58,9 +29,14 @@ RSpec.configure do |config|
   config.alias_example_to :on_post_it, :method => 'POST'
   config.alias_example_to :on_delete_it, :method => 'DELETE'
   config.before(:all) { reset_config }
-  config.color_enabled = true
+  config.color= true
 end
 
 def jruby? 
   RUBY_PLATFORM == "java"
+end
+
+def reset_config
+  SparkApi.reset
+  SparkApi.configure { |c| c.api_user = "foobar" }
 end
