d318946e94518169af46e7cd140e859153349c9d
Annika Greaney <agreaney@fbsdata.com>
2020-04-03 15:07:34 -0500

    Finished model and testing

--- lib/spark_api/models/floplan.rb
+++ lib/spark_api/models/floplan.rb
@@ -4,6 +4,22 @@ module SparkApi
       extend Subresource
       self.element_name = 'floplans'
 
+      def initialize(attributes={})
+        unless attributes['Images'].nil?
+          images = []
+          thumbnails = []
+          attributes['Images'].each do |img|
+            if img["Type"].include?('thumbnail')
+              thumbnails << img
+            else
+              images << img
+            end
+          end
+          attributes['Images'] = images
+          attributes['Thumbnails'] = thumbnails
+          super(attributes)
+        end
+      end
     end
   end
 end
