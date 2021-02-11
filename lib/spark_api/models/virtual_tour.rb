85000035953dbe14149da8801cdc609e44af08ee
Hadley <s.markoski@gmail.com>
2021-02-11 09:55:29 -0500

    RM-1085: SparkAPI Mods to create a generic "media" structure (#171)
    * RM-1127: Added media_privacy module
    
    * RM-1127: Changing how this module is brought in
    
    * RM-1127 Revert last change
    
    * RM-1127: Fixed missing require
    
    * RM-1127: Testing non-module version
    
    * RM-1127: Revert last change
    
    * RM-1127: Allowing video and virtual_tour to be savable
    
    * RM-1127: Update path support
    
    * RM-1127: Undoing last change
    
    * RM-1127: Tweaking this to provide better shared functionality.
    
    * RM-1127: Missed a spot
    
    * RM-1127: Video and virtual tour thumbnail code
    
    * RM-1127: Nixing OGP stuff for virtual tour display image
    
    * RM-1127: Added media_privacy module
    
    * RM-1127: Changing how this module is brought in
    
    * RM-1127 Revert last change
    
    * RM-1127: Fixed missing require
    
    * RM-1127: Testing non-module version
    
    * RM-1127: Revert last change
    
    * RM-1127: Allowing video and virtual_tour to be savable
    
    * RM-1127: Update path support
    
    * RM-1127: Undoing last change
    
    * RM-1127: Tweaking this to provide better shared functionality.
    
    * RM-1127: Missed a spot
    
    * RM-1127: Video and virtual tour thumbnail code
    
    * RM-1127: Nixing OGP stuff for virtual tour display image
    
    * RM-1085: Version bump
    
    Co-authored-by: Hadley Markoski <hmarkoski@fbsdata.com>

--- lib/spark_api/models/virtual_tour.rb
+++ lib/spark_api/models/virtual_tour.rb
@@ -2,6 +2,10 @@ module SparkApi
   module Models
     class VirtualTour < Base
       extend Subresource
+      include Media
+      include Concerns::Savable,
+              Concerns::Destroyable
+
       self.element_name="virtualtours"
       
 
@@ -13,6 +17,18 @@ module SparkApi
         attributes["Type"] == "unbranded"
       end
 
+      def url
+        attributes['Uri']
+      end
+
+      def description
+        attributes['Name']
+      end
+
+      def display_image
+        # Currently we have no universally good mechanism to get images for virtual tours
+        return nil
+      end
     end
   end
 end
